11311df771b7fd206fef9eabd8ad5ab7fba7c081
navidesma <esma.navid@gmail.com>
2022-03-19 11:35:56 +0330

    no major changes, trying to work with the backend

--- src/Components/PostItem/PostItem.js
+++ src/Components/PostItem/PostItem.js
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import styles from "./PostItem.module.css";
 import { usersActions } from "../../store/users-slice";
+import { useEffect } from "react";
 
 export default function PostItem(props) {
   const dispatch = useDispatch();
 
   const users = useSelector((state) => state.users);
-  const userId = props.post.userId;
-  const userName = users[`${userId}`].userName;
-  const logo = users[`${userId}`].profilePicture;
+  const userId = props.post._id;
+  // const userName = users[`${userId}`].userName;
+  // const logo = users[`${userId}`].profilePicture;
 
   const { mainUserId } = useSelector((state) => state.ui);
 
@@ -32,19 +33,19 @@ export default function PostItem(props) {
     <div className={styles.itemContainer} id={props.post.postId}>
       <div className={styles.posterInfo}>
         <Link to={`/user/${userId}`}>
-          <img src={logo} alt="poster logo" />
-          <h3>{userName}</h3>
+          {/* <img src={logo} alt="poster logo" /> */}
+          {/* <h3>{userName}</h3> */}
         </Link>
         {!(mainUserId === userId) && (
           <Button subscribed={subscribed} subHandler={subHandler} />
         )}
       </div>
       <div className={styles.imgSection}>
-        <img src={props.post.postImg} alt="post" />
+        <img src={`http://localhost:8080/${props.post.imageUrl}`} alt="post" />
       </div>
       <div className={styles.contentSection}>
-        <p>{props.post.postDesc}</p>
-        <p className="date">{props.post.postDate}</p>
+        <p>{props.post.description}</p>
+        <p className="date">{new Date(props.post.createdAt).toTimeString()}</p>
       </div>
     </div>
   );
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,23 +2,32 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import PostItem from "../../Components/PostItem/PostItem";
 
+
 export default function HomePage() {
   const [posts, setPosts] = useState([]);
-  
-  const { users } = useSelector((state) => state);
 
   useEffect(() => {
-    setPosts([]);
-    for (const userId in users) {
-      setPosts((prevState) => prevState.concat(users[`${userId}`].posts));
-    }
-  }, [users]);
+    const getPosts = async () => {
+      try {
+        const posts = await fetch("http://localhost:8080/post/get-posts");
+        if (!posts) {
+          throw new Error("no posts");
+        }
+        const toJSON = await posts.json();
+        setPosts(toJSON.posts);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPosts();
+  }, []);
 
+  console.log(posts);
   return (
     <>
-    <h2>Home</h2>
+      <h2>Home</h2>
       {posts.map((post) => (
-        <PostItem key={post.postId} post={post} />
+        <PostItem key={post._id} post={post} />
       ))}
     </>
   );
