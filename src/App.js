9d2de21e8090db3c0bb422a8fa32011fd2e7f64c
navidesma <esma.navid@gmail.com>
2022-04-16 23:03:55 +0430

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+import React, { Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./Pages/HomePage/HomePage";
-import Navbar from "./UI/Navbar/Navbar";
-import UserPage from "./Pages/UserPage/UserPage";
-import ErrorPage from "./Pages/ErrorPage/ErrorPage";
-import PostsPage from "./Pages/PostsPage/PostsPage";
-import FollowPage from "./Pages/FollowPage/FollowPage";
-import CreatePost from "./Pages/CreatePost/CreatePost";
-import SignUp from "./Pages/SignUp/SignUp";
-import Login from "./Pages/Login/Login";
-import Notification from "./UI/Notification/Notification";
-import SearchPage from "./Pages/SearchPage/SearchPage";
-import UserSettingsPage from "./Pages/UserSettingsPage/UserSettingsPage";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { initializeFollowing } from "./store/user-actions";
 
+import HomePage from "./Pages/HomePage/HomePage";
+import Navbar from "./UI/Navbar/Navbar";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import SignUp from "./Pages/SignUp/SignUp";
+import Login from "./Pages/Login/Login";
+import Notification from "./UI/Notification/Notification";
+
+const UserPage = React.lazy(() => import("./Pages/UserPage/UserPage"));
+const PostsPage = React.lazy(() => import("./Pages/PostsPage/PostsPage"));
+const FollowPage = React.lazy(() => import("./Pages/FollowPage/FollowPage"));
+const CreatePost = React.lazy(() => import("./Pages/CreatePost/CreatePost"));
+const SearchPage = React.lazy(() => import("./Pages/SearchPage/SearchPage"));
+const UserSettingsPage = React.lazy(() =>
+  import("./Pages/UserSettingsPage/UserSettingsPage")
+);
+
 function App() {
   const { notification, loggedIn } = useSelector((state) => state.ui);
-  // const {
-  //   show: showNotification,
-  //   mode: notificationMode,
-  //   header: notificationHeader,
-  //   message: notificationMessage,
-  // } = notification;
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,27 +30,29 @@ function App() {
 
   return (
     <>
-      {notification.show && <Notification props={{...notification}} />}
-      <Routes>
-        {!loggedIn && <Route path="/" element={<Login />} />}
-        {loggedIn && (
-          <>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="home" element={<HomePage />} />
-            <Route path="user" element={<UserPage />} />
-            <Route path="user/:userId" element={<UserPage />} />
-            <Route path="posts/:userId" element={<PostsPage />} />
-            <Route path="follow" element={<FollowPage />} />
-            <Route path="follow/:userId/:which" element={<FollowPage />} />
-            <Route path="add-post" element={<CreatePost />} />
-            <Route path="search-users" element={<SearchPage />} />
-            <Route path="user-settings" element={<UserSettingsPage />} />
-          </>
-        )}
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      {notification.show && <Notification props={{ ...notification }} />}
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          {!loggedIn && <Route path="/" element={<Login />} />}
+          {loggedIn && (
+            <>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="home" element={<HomePage />} />
+              <Route path="user" element={<UserPage />} />
+              <Route path="user/:userId" element={<UserPage />} />
+              <Route path="posts/:userId" element={<PostsPage />} />
+              <Route path="follow" element={<FollowPage />} />
+              <Route path="follow/:userId/:which" element={<FollowPage />} />
+              <Route path="add-post" element={<CreatePost />} />
+              <Route path="search-users" element={<SearchPage />} />
+              <Route path="user-settings" element={<UserSettingsPage />} />
+            </>
+          )}
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
       {loggedIn && <Navbar />}
     </>
   );
